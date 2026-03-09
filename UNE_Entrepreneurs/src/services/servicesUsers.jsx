0cf7d552c4e7004307db4fc4ff6b36be446d999f
tayroarce-lab <tayroarce@icloud.com>
2026-03-09 13:20:57 -0600

    Estructura base del proyecto

---
+++ UNE_Entrepreneurs/src/services/servicesUsers.jsx
@@ -0,0 +1,83 @@
+//PATCH
+async function patchUsuarios(usuario,id){
+
+       try {
+
+        const respuesta = await fetch("http://localhost:3001/usuarios/"+id,{
+            method:"PATCH",
+            headers:{
+                "Content-Type":"application/json"
+            },
+            body:JSON.stringify(usuario)
+
+        })
+
+        const datosUsuarios= await respuesta.json();
+
+        return datosUsuarios;
+        
+    } catch (error) {
+        
+        console.error("Error al actualizar los cambios", error);
+    }
+}
+
+
+
+//DELETE
+
+
+
+async function deleteUsuarios(id){
+
+       try {
+
+        const respuesta = await fetch("http://localhost:3001/usuarios/"+id,{
+            method:"DELETE",
+        })
+
+        const datosUsuarios= await respuesta.json();
+
+        return datosUsuarios;
+        
+    } catch (error) {
+        
+        console.error("Error al Eliminar el registro", error);
+    }
+}
+
+
+
+
+
+async function getUser() {
+    try {
+        const respuesta = await fetch("http://localhost:3001/usuarios")
+        const datos = await respuesta.json();
+        return datos;
+    } catch (error) {
+        console.error("Error al obtener los usuarios", error);
+    }
+}
+
+async function postUser(usuario) {
+    try {
+        const respuesta = await fetch("http://localhost:3001/usuarios", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(usuario)
+        })
+        const datos = await respuesta.json();
+        return datos;
+    } catch (error) {
+        console.error("Error al registrar el usuario", error);
+    }
+}
+
+export default { patchUsuarios, deleteUsuarios, getUser, postUser }
+
+
+
+
